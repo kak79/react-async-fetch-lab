71f9a4a738825c80663a4db7818af6ef7d00ccb9
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-15 16:39:24 -0500

    commit tree ec405cf0716bfc6d8ab25ccf2f895ffc588e92e5

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import React from "react"
 
 class App extends React.Component {
 
+  state = {
+    list: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          list: data.people
+        })
+      })
+  }
 
 
+  render() {
+    return(
+      <div>
+        {this.state.list.map(dude => dude.name + ' ')}
+      </div>
+    )
+  }
 
 
 }
